445f2e7294c7b3709a141eb33091eb1218d0ae65
Luis Leon <lleon95@gmail.com>
2020-02-10 17:55:37 +0100

    Add function selection feature from arguments

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 
 def fx(x):
     '''
@@ -8,5 +9,14 @@ def fx(x):
 
 # Main execution line
 xval = [x * 0.1 for x in list(range(-50,51,1))]
-yval = fx(xval)
 
+# Set default function
+function_collection = [fx]
+function_number = 1
+
+if len(sys.argv) > 1:
+    function_choose = sys.argv[1]
+    if function_choose > 0 and function_choose <= len(function_collection):
+        function_number = int(sys.argv[1])
+    
+yval = function_collection[function_number - 1](xval)
