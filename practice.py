bbdd4622f3605306448206b684fe118b4dc84dfd
Luis Leon <lleon95@gmail.com>
2020-02-10 18:47:58 +0100

    Add polynomial function (number 2)

--- practice.py
+++ practice.py
@@ -2,17 +2,25 @@
 import sys
 import matplotlib.pyplot as plt
 
+# Function 1
 def fx(x):
     '''
     Initial function f(x)
     '''
     return x
 
+# Function 2
+def fx2(x):
+    '''
+    Polynomial function: f(x) = x**2
+    '''
+    return [ xi**2 for xi in x ]
+
 # Main execution line
 xval = [x * 0.1 for x in list(range(-50,51,1))]
 
 # Set default function
-function_collection = [fx]
+function_collection = [fx, fx2]
 function_number = 1
 
 if len(sys.argv) > 1:
